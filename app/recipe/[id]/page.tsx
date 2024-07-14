53f1062b8cd819b5599b59c4d5c45652391a517f
Elias Nau <elias.nau@outlook.de>
2024-07-14 15:23:59 +0200

    MOD recipe Page

--- app/recipe/[id]/page.tsx
+++ app/recipe/[id]/page.tsx
@@ -1,10 +1,57 @@
-export default function Recipe({ params }: { params: { id: string } }) {
+import Image from "next/image";
+
+export default async function Page({ params }) {
+  const { id } = params;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1>Recipe: {params.id}</h1>
-        <p>This is the recipe for {params.id}.</p>
+    <div className="container mx-auto my-20">
+      <div className="flex border-2 border-gray-300 cursor-pointer hover:border-black p-4">
+        {/* Image Section on the Left */}
+        <div className="relative w-[50%] h-[500px] mr-8">
+          <Image
+            src="/static"
+            layout="fill"
+            objectFit="cover"
+            alt="Meal Image"
+          />
+        </div>
+
+        {/* Information Section on the Right */}
+        <div className="w-[50%]">
+          <h1 className="bg-white py-4 text-gray-500 font-semibold text-2xl text-center mb-4">
+            Food
+          </h1>
+
+          {/* Ingredients Card */}
+          <div className="bg-white p-4 mb-4 border border-gray-300 rounded">
+            <h2 className="text-xl font-semibold mb-2">Ingredients:</h2>
+            <div className="mb-2">
+              <span className="font-semibold">Hallo:</span> 2g
+            </div>
+          </div>
+
+          {/* Steps Card */}
+          <div className="bg-white p-4 mb-4 border border-gray-300 rounded">
+            <h2 className="text-xl font-semibold mb-2">Steps:</h2>
+            <ol className="list-decimal pl-4">
+              <li>1. Kochen</li>
+              <li>2. Essen</li>
+            </ol>
+          </div>
+
+          {/* YouTube Video */}
+          <div className="mb-4">
+            <iframe
+              width="100%"
+              height="315"
+              src="https://youtube.com/embed/dQw4w9WgXcQ"
+              title="YouTube Video"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
